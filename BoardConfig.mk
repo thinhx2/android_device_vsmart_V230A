fea08c9aba89f8d6bb3a369b599c29ca73f6d8bb
thinhx2 <nthinh1102@gmail.com>
2020-03-28 20:11:44 +0700

    Bee 3 use cpu 64bit with 32bit mode

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,10 +17,10 @@ TARGET_OTA_ASSERT_DEVICE := V230A
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_USES_64_BIT_BINDER := true
 
 # Audio
